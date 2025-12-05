004bc8524ab9fb554b5d91665f25316f7e894bb2
achahine78 <achahine78@gmail.com>
2025-12-05 09:23:32 -0500

    day 3

--- js/day3/index.js
+++ js/day3/index.js
@@ -1,42 +1,66 @@
 const readLinesFromFile = require("../fileReader");
 
-const getMaxJoltage = (processedBank) => {
-    let firstMax = -Infinity;
-    let firstMaxIndex = -1;
-    for (let i = 0; i < processedBank.length - 1; i++) {
-        if (processedBank[i] > firstMax) {
-            firstMax = processedBank[i];
-            firstMaxIndex = i;
+const maxBetween = (arr, start, end) => {
+    let max = -Infinity;
+    let maxIndex = -1;
+
+    const s = Math.max(0, start);
+    const e = Math.min(arr.length - 1, end);
+
+    for (let i = s; i <= e; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            maxIndex = i;
         }
     }
 
-    let secondMax = -Infinity;
+    return {
+        max,
+        maxIndex,
+    };
+};
 
-    for (let i = firstMaxIndex + 1; i < processedBank.length; i++) {
-        if (processedBank[i] > secondMax) {
-            secondMax = processedBank[i];
-        }
+const getMaxJoltage = (processedBank, numberOfBatteries) => {
+    let pointers = new Array(numberOfBatteries).fill(0);
+    pointers = pointers.map(
+        (_, i) => processedBank.length - numberOfBatteries + i
+    );
+    let previousIndex = 0;
+    for (let i = 0; i < pointers.length; i++) {
+        const { max, maxIndex } = maxBetween(
+            processedBank,
+            previousIndex,
+            pointers[i]
+        );
+        previousIndex = maxIndex + 1;
+        pointers[i] = maxIndex;
     }
 
-    return `${firstMax}${secondMax}`;
+    let value = "";
+
+    pointers.forEach((pointer) => {
+        value += processedBank[pointer];
+    });
+
+    return value;
 };
 
 const partOne = (linesArray) => {
     let count = 0;
     for (const bank of linesArray) {
         const processedBank = bank.split("").map(Number);
-        const maxJoltage = getMaxJoltage(processedBank);
+        const maxJoltage = getMaxJoltage(processedBank, 2);
         count += Number(maxJoltage);
     }
 
     return count;
 };
 
-const partTwo = (inputArray) => {
+const partTwo = (linesArray) => {
     let count = 0;
     for (const bank of linesArray) {
         const processedBank = bank.split("").map(Number);
-        const maxJoltage = getMaxJoltage(processedBank);
+        const maxJoltage = getMaxJoltage(processedBank, 12);
         count += Number(maxJoltage);
     }
 
@@ -48,8 +72,7 @@ readLinesFromFile(filePath, (err, linesArray) => {
     if (err) {
         console.error("Error reading file:", err);
     } else {
-        // console.log(linesArray);
         console.log("Part 1:", partOne(linesArray));
-        //console.log("Part 2:", partTwo(inputArray));
+        console.log("Part 2:", partTwo(linesArray));
     }
 });
